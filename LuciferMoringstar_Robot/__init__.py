946af89f8667a66f5419d462da79b9d73a771f20
Akashvro <90610484+Akashvro@users.noreply.github.com>
2022-01-29 11:36:15 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,12 +30,11 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [คkคŞh](311b72ad_9181663614e068ba)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ channel : [Click Me](https://t.me/MM_Films)
 """
